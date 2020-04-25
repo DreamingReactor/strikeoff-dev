68af234d12f111cac9184c74e1b341e02b729cec
DreamingReactor <srivastavanitin283@gmail.com>
2020-04-26 04:34:59 +0530

    login premitive

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ def test():
 def register():
     body = request.get_json()
     user_no = str(mongo.db.users.find().count())
-    print(user_no)
     email = body.get('email', '')
     password = body.get('password', '')
     name = body.get('name', '')
@@ -39,4 +38,26 @@ def register():
         user_id = mongo.db.users.insert(data)
         response = json.dumps({'sucess': True, 'id': str(user_id)})
         status_code = 201
+    return Response(response, status=status_code, mimetype='application/json')
+
+@app.route('/login', methods = ['POST'])
+def login():
+    body = request.get_json()
+    user_id = body.get('user_id')
+    password = body.get('password')
+    if user_id.isdigit():
+        user = mongo.db.users.find_one({"mobile": user_id})
+    else:
+        user = mongo.db.users.find_one({"email": user_id})
+    if user:
+        if password == user["password"]:
+            user['_id'] = str(user['_id'])
+            response = json.dumps({'sucess': True, 'message': 'Successfully logged in.', 'user_data': user})
+            status_code = 200
+        else:
+            response = json.dumps({'sucess': False, 'message': 'Wrong Password.'})
+            status_code = 403
+    else:
+        response = json.dumps({'sucess': False, 'message': 'User doesn\'t exist.'})
+        status_code = 403
     return Response(response, status=status_code, mimetype='application/json')
